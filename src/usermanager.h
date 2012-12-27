f904661db0843a274a5691b5cebd7afa555654e2
Alex Fiestas <afiestas@kde.org>
2012-12-27 06:07:42 +0100

    First import of what will be the new User Manager kcm

---
+++ src/usermanager.h
@@ -0,0 +1,39 @@
+/*************************************************************************************
+ *  Copyright (C) 2012 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#ifndef USER_MANAGER_H
+#define USER_MANAGER_H
+
+#include <KCModule>
+
+namespace Ui
+{
+    class KCMUserManager;
+};
+class UserManager : public KCModule
+{
+    Q_OBJECT
+    public:
+        explicit UserManager(QWidget *parent, const QVariantList& args);
+        virtual ~UserManager();
+
+    private:
+        Ui::KCMUserManager* m_ui;
+};
+
+#endif // USER-MANAGER_H_
