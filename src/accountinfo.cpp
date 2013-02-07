cffabb342e37830cc869c7d65dfc9d3f9a5aed81
Àlex Fiestas <afiestas@kde.org>
2013-02-07 20:12:43 +0100

    Add an AccountInfo widget
    The AccountInfo widget will take a model item and fill itself with data
    comming from usermanager server.
    
    I'm not sure if I want it to save the data as well, but at least it will
    manage if any information changes.

--- src/accountinfo.cpp
+++ src/accountinfo.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************************
- *  Copyright (C) 2012 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *  Copyright (C) 2013 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
  *                                                                                   *
  *  This program is free software; you can redistribute it and/or                    *
  *  modify it under the terms of the GNU General Public License                      *
@@ -16,54 +16,19 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
  *************************************************************************************/
 
-#include "usermanager.h"
-#include "ui_kcm.h"
-#include "ui_account.h"
 #include "accountinfo.h"
-
+#include "ui_account.h"
 #include "lib/accountmodel.h"
-#include "lib/modeltest.h"
 
-#include <QtCore/QDebug>
-#include <QtGui/QListView>
-#include <QtGui/QStackedLayout>
-
-#include <kpluginfactory.h>
-
-K_PLUGIN_FACTORY(UserManagerFactory, registerPlugin<UserManager>();)
-K_EXPORT_PLUGIN(UserManagerFactory("user_manager", "user_manager"))
-
-UserManager::UserManager(QWidget* parent, const QVariantList& args) 
- : KCModule(UserManagerFactory::componentData(), parent)
- , m_ui(new Ui::KCMUserManager)
- , m_layout(new QStackedLayout)
+AccountInfo::AccountInfo(AccountModel* model, QWidget* parent, Qt::WindowFlags f)
+ : QWidget(parent, f)
+ , m_model(model)
 {
-    Q_UNUSED(args);
-    m_ui->setupUi(this);
-    m_ui->accountInfo->setLayout(m_layout);
-
-    AccountModel* model = new AccountModel(this);
-    QItemSelectionModel* selectionModel = new QItemSelectionModel(model);
-    connect(selectionModel, SIGNAL(currentChanged(QModelIndex,QModelIndex)), SLOT(currentChanged(QModelIndex,QModelIndex)));
-
-    m_ui->userList->setModel(model);
-    m_ui->userList->setSelectionModel(selectionModel);
-
-    ModelTest* test = new ModelTest(model, 0);
-
-    AccountInfo *widget = new AccountInfo(model);
-
-    m_layout->addWidget(widget);
-    m_layout->setCurrentWidget(widget);
+    Ui::AccountInfo *info = new Ui::AccountInfo();
+    info->setupUi(this);
 }
 
-UserManager::~UserManager()
-{
-}
-
-void UserManager::currentChanged(const QModelIndex& selected, const QModelIndex& previous)
+AccountInfo::~AccountInfo()
 {
 
 }
-
-#include "usermanager.moc"
