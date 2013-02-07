3c0431cfe3745deb0c0338ed2ea32bdd4c6e6095
Àlex Fiestas <afiestas@kde.org>
2013-02-07 22:04:56 +0100

    And export to the right catalog

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT *.cpp -o $podir/user-manager.pot
+$XGETTEXT *.cpp -o $podir/user_manager.pot
